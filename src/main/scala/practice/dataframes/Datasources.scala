e03406d7faaf841f347210b44213f8caeb2622b7
Bartosz Gajda <bg@bartoszgajda.com>
2020-02-22 22:33:53 +0000

    Data sources exercises.

--- src/main/scala/practice/dataframes/Datasources.scala
+++ src/main/scala/practice/dataframes/Datasources.scala
@@ -1,7 +1,7 @@
 package practice.dataframes
 
 import org.apache.spark.sql.{SaveMode, SparkSession}
-import org.apache.spark.sql.types.{DoubleType, LongType, StringType, StructField, StructType}
+import org.apache.spark.sql.types.{DateType, DoubleType, LongType, StringType, StructField, StructType}
 import practice.dataframes.DataframesBasics.spark
 
 object Datasources extends App {
@@ -18,7 +18,7 @@ object Datasources extends App {
     StructField("Horsepower", LongType),
     StructField("Weight_in_lbs", LongType),
     StructField("Acceleration", DoubleType),
-    StructField("Year", StringType),
+    StructField("Year", DateType),
     StructField("Origin", StringType)
   ))
 
@@ -55,4 +55,63 @@ object Datasources extends App {
     .format("json")
     .mode(SaveMode.Overwrite)
     .save("src/main/resources/data/cars_duplicate.json")
+
+  // JSON flags
+  spark.read
+    .schema(carsSchema)
+    .option("dateFormat", "YYYY-MM-dd")
+    .option("allowSingleQuotes", "true")
+    .option("compression", "uncompressed") // bzip2, gzip, lz4, snappy or deflate
+    .json("src/main/resources/data/cars.json")
+
+  // CSV
+  val stocksSchema = StructType(Array(
+    StructField("symbol", StringType),
+    StructField("date", DateType),
+    StructField("price", DoubleType)
+  ))
+  spark.read
+    .schema(stocksSchema)
+    .option("dateFormat", "MMM dd YYYY")
+    .option("header", "true")
+    .option("sep", ",")
+    .option("nullValue", "")
+    .csv("src/main/resources/data/stocks.csv")
+
+  // Parquet
+  carsDf.write
+    .mode(SaveMode.Overwrite)
+    .save("src/main/resources/data/cars.parquet")
+
+  // Text files
+  spark.read
+    .text("src/main/resources/data/sampleTextFile.txt").show()
+
+  // Reading from a remote DB
+  spark.read
+    .format("jdbc")
+    .option("driver", "org.postgresql.Driver")
+    .option("url", "jdbc:postgresql://localhost:5432/rtjvm")
+    .option("user", "docker")
+    .option("password", "docker")
+    .option("dbtable", "public.employees")
+    .load()
+
+  /**
+    * Exercise
+    * - read movies df
+    * - save as tab separated csv
+    * - save as a snappy parquet
+    */
+  val movies = spark.read
+    .option("inferSchema", "true")
+    .json("src/main/resources/data/movies.json")
+
+  movies.write
+    .format("csv")
+    .option("header", "true")
+    .option("sep", "\t")
+    .save("src/main/resources/data/movies.csv")
+
+  movies.write.save("src/main/resources/data/movies.parquet")
 }
