330de97760b8cfc7148bfcec91663276d091e032
Bartosz Gajda <bg@bartoszgajda.com>
2020-03-02 20:34:01 +0000

    RDDs.

---
+++ src/main/scala/practice/lowlevel/RDDS.scala
@@ -0,0 +1,50 @@
+package practice.lowlevel
+
+import org.apache.spark.sql.SparkSession
+
+import scala.io.Source
+
+object RDDs extends App {
+  val spark = SparkSession.builder()
+    .appName("RDDs")
+    .master("local[1]")
+    .getOrCreate()
+
+  val sc= spark.sparkContext
+
+  // Parallelling existing collection
+  val numbers = 1 to 100000
+  val numbersRDD = sc.parallelize(numbers)
+
+  // Reading from files
+  case class StockValue(company: String, date: String, price: Double)
+  def readStocks(filename: String) = {
+    Source.fromFile(filename)
+      .getLines()
+      .drop(1)
+      .map(line => line.split(","))
+      .map(tokens => StockValue(tokens(0), tokens(1), tokens(2).toDouble))
+      .toList
+  }
+
+  val stocksRdd = sc.parallelize(readStocks("src/main/resources/data/stocks.csv"))
+
+  // Reading from files v2
+  val stocksRdd2 = sc.textFile("src/main/resources/data/stocks.csv")
+    .map(line => line.split(","))
+    .filter(tokens => tokens(0).toUpperCase() == tokens(0))
+    .map(tokens => StockValue(tokens(0), tokens(1), tokens(2).toDouble))
+
+  // Reading from DF
+  val stocksDf = spark.read
+    .option("header", "true")
+    .option("inferSchema", "true")
+    .csv("src/main/resources/data/stocks.csv")
+
+  import spark.implicits._
+  val stocksDs = stocksDf.as[StockValue]
+  val stocksRdd3 = stocksDs.rdd
+
+  // Rdd to Df
+  val numbersDf = numbersRDD.toDF("numbers")
+}
